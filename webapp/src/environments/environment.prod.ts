7c65c9df7d0f860dc8167784d7cb849b3f7b2c3e
Hari Chandrasekhar <chandrasekharh@kritek.org>
2020-02-28 19:31:56 -0700

    Fixed issues during prod build. Set environment for prod

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,13 +1,17 @@
+const projectId = `kaoc-aa17d`;
+const hostURl = `kaoc.app`;
+
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyB6HrwLzZ7IG_vBVW3i5PKPtIAnIHkPEd0",
-    authDomain: "kaoc-aa17d.firebaseapp.com",
-    databaseURL: "https://kaoc-aa17d.firebaseio.com",
-    projectId: "kaoc-aa17d",
-    storageBucket: "kaoc-aa17d.appspot.com",
-    messagingSenderId: "766727019490",
-    appId: "1:766727019490:web:98fd3ae1b70e267891f7bb",
-    measurementId: "G-J1SQY9HB34"
-  }
+    production: false,
+    firebase: {
+        apiKey            : `AIzaSyB6HrwLzZ7IG_vBVW3i5PKPtIAnIHkPEd0`,
+        authDomain        : `${hostURl}`,
+        databaseURL       : `https://${projectId}.firebaseio.com`,
+        projectId         : `${projectId}`,
+        storageBucket     : `${projectId}.appspot.com`,
+        messagingSenderId : '766727019490',
+        appId             : '1:766727019490:web:98fd3ae1b70e267891f7bb',
+        measurementId     : 'G-J1SQY9HB34',
+        functionURL       : `https://${hostURl}/api`
+    }
 };
