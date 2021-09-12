50c1ce6b4bb04a5f4fd4148c5740fbbb7e292691
maneesha.s <maneesha.s@objectstream.com>
2021-09-12 13:17:32 +0530

    Fixing Issue #15 Sorting Option Missing in the view members page.

--- webapp/src/app/secured/admin/list-member-profiles/list-member-profiles.component.ts
+++ webapp/src/app/secured/admin/list-member-profiles/list-member-profiles.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import { MemberService } from '../../member.service';
-import { Member } from '../../Member';
-import { Router } from '@angular/router';
+import {MemberService} from '../../member.service';
+import {Member} from '../../Member';
+import {Router} from '@angular/router';
+import _ from 'lodash';
 import {MatDialog, MatTableDataSource} from '@angular/material';
 
 @Component({
@@ -16,31 +17,46 @@ export class ListMemberProfilesComponent implements OnInit {
   members: Member[];
   membersCpy: Member[];
   pageOfItems: Array<any>;
+  isAsc: boolean;
   @Input() enableFilter: boolean;
+
   constructor(private memberService: MemberService,
-              private router: Router) { }
+              private router: Router) {
+  }
 
   ngOnInit() {
-      this.memberService.getAllMembers().subscribe(members => {
-          // console.log (members);
-          this.membersCpy = this.members = members;
-      });
+    this.memberService.getAllMembers().subscribe(members => {
+      this.members = members;
+      this.membersCpy = this.members = _.sortBy(this.members, ['firstName']);
+      this.isAsc = true;
+    });
   }
+
+  sort() {
+    if (this.isAsc) {
+      this.membersCpy = this.members =  _.orderBy(this.members, ['firstName'], ['desc']);
+    } else {
+      this.membersCpy = this.members =  _.sortBy(this.members, ['firstName']);
+    }
+    this.isAsc = !this.isAsc;
+  }
+
   getMembershipDetails(member: Member) {
     this.router.navigate(['secured/admin/memberprofile', member.docId]);
   }
+
   onChangePage(pageOfItems: Array<any>) {
     // update current page of items
     this.pageOfItems = pageOfItems;
   }
+
   applyFilter(filterValue: string) {
     // this.members.filter = filterValue.trim().toLowerCase();
-    this.members = this.pageOfItems = this.membersCpy.
-    filter(t => (
+    this.members = this.pageOfItems = this.membersCpy.filter(t => (
       (t.firstName.toLowerCase().includes(filterValue.trim().toLowerCase()) || t.firstName.includes(filterValue))
-         || (t.lastName.toLowerCase().includes(filterValue.trim().toLowerCase()) || t.lastName.includes(filterValue))
-         || (t.emailId ? (t.emailId.toLowerCase().includes(filterValue.trim().toLowerCase()) || t.emailId.includes(filterValue)) : null )
-         || (t.phoneNumber ? (t.phoneNumber.toString().includes(filterValue.trim())) : null)
-      ));
+      || (t.lastName.toLowerCase().includes(filterValue.trim().toLowerCase()) || t.lastName.includes(filterValue))
+      || (t.emailId ? (t.emailId.toLowerCase().includes(filterValue.trim().toLowerCase()) || t.emailId.includes(filterValue)) : null)
+      || (t.phoneNumber ? (t.phoneNumber.toString().includes(filterValue.trim())) : null)
+    ));
   }
 }
