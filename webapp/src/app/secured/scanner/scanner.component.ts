8db0179e6e82a5bcd28b27a259041fa7d608c95d
Vimal <vimal@helloinfinity.us>
2021-12-06 02:35:59 -0600

    Update scanner.component.ts

--- webapp/src/app/secured/scanner/scanner.component.ts
+++ webapp/src/app/secured/scanner/scanner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ADMIN_VIEW_MEMBER_PROFILE_PREFIX } from '../../URLConstants'
+import { ADMIN_VIEW_MEMBER_PROFILE_PREFIX } from '../../URLConstants';
 
 const KAOC_MEMBER_ID_REGEX = /^kaocMemberId:(?<memberId>.+)$/;
 const KAOC_MEMBER_EVENT_CHECKIN_REGEX = /^kaocEventCheckIn:kaocMemberId:(?<memberId>.+):kaocEventId:(?<eventId>.+)$/;
@@ -20,19 +20,19 @@ export class ScannerComponent implements OnInit {
   }
 
   onCodeResult(result: string) {
-    if(result) {
+    if (result) {
       this.scanResult = result;
-      let memberIdMatch = KAOC_MEMBER_ID_REGEX.exec(result);
-      if(memberIdMatch) {
-          let memberId = memberIdMatch.groups.memberId;
+      const memberIdMatch = KAOC_MEMBER_ID_REGEX.exec(result);
+      if (memberIdMatch) {
+          const memberId = memberIdMatch.groups.memberId;
           this.router.navigate([`secured/${ADMIN_VIEW_MEMBER_PROFILE_PREFIX}`, memberId]);
       } else {
-        let memberEventCheckInMatch = KAOC_MEMBER_EVENT_CHECKIN_REGEX.exec(result);
-        if(memberEventCheckInMatch) {
-          let memberId = memberEventCheckInMatch.groups.memberId;
-          let eventId = memberEventCheckInMatch.groups.eventId;
+        const memberEventCheckInMatch = KAOC_MEMBER_EVENT_CHECKIN_REGEX.exec(result);
+        if (memberEventCheckInMatch) {
+          const memberId = memberEventCheckInMatch.groups.memberId;
+          const eventId = memberEventCheckInMatch.groups.eventId;
           // TODO
-          //this.router.navigate(['secured/admin/eventCheckIn', memberId, eventId]);
+          // this.router.navigate(['secured/admin/eventCheckIn', memberId, eventId]);
         } else {
           console.error(`Unsupported QR Code ${result}`);
         }
